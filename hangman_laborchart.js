9b75ca8951e820972c74198a16d2161364e681c2
Trung Pham <phamanhtrung@gmail.com>
2021-01-13 01:40:39 -0600

    add js and html files

---
+++ hangman_laborchart.js
@@ -0,0 +1,34 @@
+let words = ['hangman', 'laborchart', 'consequences']
+let w = words[Math.round(Math.random() * (words.length - 1))]
+let guess = [];
+for (let i = 0; i < w.length; i++) {
+    guess.push('_')
+}
+let guessLeft = w.length
+console.log(guess.join(' '));
+
+while (guessLeft && guess.includes('_')) {
+    let g = prompt('Guess a letter: ');
+    is_correct = w.includes(g);
+    console.log('You guessed: ' + g);
+    
+    if (is_correct) {
+        console.log('%c Correct guess!', 'color: green')
+    } else {
+        console.log('%c Wrong guess.', 'color: red')
+    }
+
+    for (let i = 0; i < w.length; i++) {
+        if (w[i] === g) {
+            guess[i] = g
+        }
+    }
+    console.log(guess.join(' '));
+    guessLeft--;
+}
+
+if (guess.includes('_')) {
+    console.log('%c Sorry, you did not finish the Hangman.', 'background: red');
+} else {
+    console.log('%c You win. Congrats!', 'background: green');
+}
